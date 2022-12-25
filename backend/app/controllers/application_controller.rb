0a9dc0e2c5cae33646a70c633ddb14744322bf3e
Nathan Kwon <nathan.kwon.8.18@gmail.com>
2022-12-24 20:34:56 -0800

    added basic auth

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,6 +1,31 @@
 class ApplicationController < ActionController::API
   before_action :snake_case_params
 
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def require_logged_in
+    redirect_to new_session_url unless logged_in?
+  end
+
+  def require_logged_out
+    redirect_to users_url if logged_in?
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login(user)
+    session[:session_token] = user.reset_session_token
+  end
+
+  def logout
+    current_user.reset_session_token if logged_in?
+    session[:session_token] = nil
+    @current_user = nil
+  end
 private
   def snake_case_params
     params.deep_transform_keys!(&:underscore)
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -12,5 +12,62 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+
+# validations
   has_secure_password
+  validates :username,
+    uniqueness: true, 
+    length: { in: 7..30 }, 
+    format: { without: URI::MailTo::EMAIL_REGEXP, 
+      message: "Your username cannot be your email." 
+    }
+  validates :email,
+    uniqueness: true, 
+    length: { in: 3..255 }, 
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :session_token, 
+    presence: true, 
+    uniqueness: true
+  validates :password, length: { in: 8..255 }, allow_nil: true
+
+# authorization methods 
+  def self.find_by_credentials(username, password)
+    @user = User.find_by(username: username)
+    if @user && @user.is_password?(password)
+      return @user
+    else
+      return nil
+    end
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    bcrypt_obj = BCrypt::Password.new(self.password_digest)
+    bcrypt_obj.is_password?(password)
+  end
+
+  def reset_session_token
+    self.session_token = generate_unique_session_token
+    self.save!
+    self.session_token
+  end
+
+  def self.ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
+
+  private
+
+  def generate_unique_session_token
+    token = SecureRandom::urlsafe_base64
+    while User.exists(session_token: token)
+      token = SecureRandom::urlsafe_base64
+    end
+    token
+  end
+
 end
