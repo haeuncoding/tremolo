6af6567f690ddb2d1271b1b9888315c2a21c448d
Nathan Kwon <nathan.kwon.8.18@gmail.com>
2022-12-27 13:09:21 -0800

    adding store to user auth

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,10 @@
 
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import session from './session'
 
 export const rootReducer = combineReducers({
-
+  session
 })
 
 let enhancer;
