3eca1eeb3667e3649750b2331d2f8b9f9b3ebe8f
Nathan Kwon <nathan.kwon.8.18@gmail.com>
2022-12-28 13:49:42 -0800

    adding icons to navbar

--- frontend/src/components/Navigation/SearchButton.js
+++ frontend/src/components/Navigation/SearchButton.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
+import SearchIcon from '../../assets/search-line-icon.svg'
 
 
 const SearchButton = () => {
 return (
-        <div style={{fontSize: "15px" }}>
-        <i className="fa-regular fa-magnifying-glass"></i>
-      </div>
+  <div>
+    <img style={{ width: "2vw", height: "auto" }} src={SearchIcon} />
+  </div>
 )}
 
 export default SearchButton;
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -8,6 +8,10 @@ import SignupFormModal from '../SignupFormPageModal';
 import logo from '../../assets/TremoloLogo.png'
 import logoColor from '../../assets/TremoloLogo_Color.png'
 import SearchButton from './SearchButton';
+import NotifBell from '../../assets/bell-line-icon.svg';
+import FeedGrid from '../../assets/gallery-tiles-view-outline-icon.svg';
+import CartIcon from '../../assets/shopping-cart-icon.svg';
+import WatchList from '../../assets/view-icon.svg'
 
 function Navigation() {
   const sessionUser = useSelector(state => state.session.user);
@@ -44,14 +48,49 @@ function Navigation() {
             />
         </NavLink>
       </li>
-      <li>
+      <li class="search-container">
         <input type="text" class="search-bar" placeholder="Look for used and new gear here..." />
-        <SearchButton />
+        <SearchButton class="search-bar" id="search-button" />
       </li>
       <li>
         <button>
           Create a New Listing
         </button>
+      </li>
+      <li>
+        <img class="nav-icon"
+          id="watchlist" 
+          src={WatchList}
+          // onMouseOver={e => (e.currentTarget.src = logoColor)} 
+          // onMouseOut={e => (e.currentTarget.src = logo)} 
+          />
+      </li>
+      <li>
+        <img class="nav-icon"
+          id="feed-grid" 
+          src={FeedGrid}
+          // onMouseOver={e => (e.currentTarget.src = logoColor)} 
+          // onMouseOut={e => (e.currentTarget.src = logo)} 
+          />
+      </li>
+      <li>
+        <img class="nav-icon"
+          id="cart-icon" 
+          src={CartIcon}
+          // onMouseOver={e => (e.currentTarget.src = logoColor)} 
+          // onMouseOut={e => (e.currentTarget.src = logo)} 
+          />
+      </li>
+      <li>
+        <img class="nav-icon"
+          id="notif-bell" 
+          src={NotifBell}
+          // onMouseOver={e => (e.currentTarget.src = logoColor)} 
+          // onMouseOut={e => (e.currentTarget.src = logo)} 
+          />
+      </li>
+      <li>
+        {/* <WatchList class="nav-icon" /> */}
       </li>
       {sessionLinks}
     </ul>
