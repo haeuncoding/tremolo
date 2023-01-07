b8630e1fe78f3f88b6b7015f0313e4568fe5f4ad
Nathan Kwon <nathan.kwon.8.18@gmail.com>
2023-01-07 15:43:30 -0800

    finished splash screen

--- frontend/src/components/SPLASH/SplashScreen.js
+++ frontend/src/components/SPLASH/SplashScreen.js
@@ -1,9 +1,21 @@
 import SplashScreenImg from '../../assets/SplashScreenNoWords.png'
+import { Link } from 'react-router-dom'
+import './SplashScreen.css'
 
 const SplashScreen = () => {
 
   return (
     <div id="splash-container">
+      <div id="splash-text-container">
+        <h1 id="splash-title">New Year, New Gear</h1>
+        <br />
+        <h3 id="splash-caption">That means savings on items from Chise Blass, Ernie Balls, Bender, and more!</h3> 
+        <Link id="link-button-container" to='/listings'>
+          <button id="shop-now-button">
+            Shop Now
+          </button>       
+       </Link>
+      </div>
       <img src={SplashScreenImg} id="splash-screen"/>                
     </div>
   )
